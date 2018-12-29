4f7f86f2aa4dbde17359e4db8825bace29d1048f
Jeroen Ooms <jeroenooms@gmail.com>
2018-12-29 18:19:30 +0100

    Add gpgme backport

--- Formula/gpgme.rb
+++ Formula/gpgme.rb
@@ -1,16 +1,15 @@
 class Gpgme < Formula
   desc "Library access to GnuPG"
   homepage "https://www.gnupg.org/related_software/gpgme/"
-  url "https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.11.1.tar.bz2"
-  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gpgme/gpgme-1.11.1.tar.bz2"
-  sha256 "2d1b111774d2e3dd26dcd7c251819ce4ef774ec5e566251eb9308fa7542fbd6f"
+  url "https://www.gnupg.org/ftp/gcrypt/gpgme/gpgme-1.12.0.tar.bz2"
+  mirror "https://www.mirrorservice.org/sites/ftp.gnupg.org/gcrypt/gpgme/gpgme-1.12.0.tar.bz2"
+  sha256 "b4dc951c3743a60e2e120a77892e9e864fb936b2e58e7c77e8581f4d050e8cd8"
 
   bottle do
     cellar :any
-    sha256 "a91bc4690b429612ec4c855d0f7875f2857ab7dcb6638453c23f390fae9c9e31" => :mojave
-    sha256 "8a4e1bbd9ce26be05a9f0875c7f258c36fc3387d6cf628684c6f5427a63402ca" => :high_sierra
-    sha256 "90e45544ee3e670f1f8e808b504057d360a260c33cf34682e3a3686b6a4cda83" => :sierra
-    sha256 "88f9c39143597eadd5a408bc8e5b1580014358977d8f3a7cb89c0ac6372510db" => :el_capitan
+    rebuild 1
+    root_url "https://jeroen.github.io/bottles"
+    sha256 "74b6fbc400e6b513cd42cfe8ab2379097dea11dadeb22b814d940c49e9ee06df" => :el_capitan
   end
 
   depends_on "swig" => :build
