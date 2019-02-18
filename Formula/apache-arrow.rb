58de1b8c28b41bbfe1cbe69821809ba831c7c610
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-18 22:09:47 +0100

    rebuild arrow, again

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -5,35 +5,43 @@ class ApacheArrow < Formula
   sha256 "34dae7e4dde9274e9a52610683e78a80f3ca312258ad9e9f2c0973cf44247a98"
 
   bottle do
-    cellar :any
-    rebuild 2
+    rebuild 3
+    cellar :any_skip_relocation
     root_url "https://jeroen.github.io/bottles"
-    sha256 "4b6cbf95f93b647d4d4e201176aab90b778f84c4b255438337706fc13768c97c" => :el_capitan
+    sha256 "c02a0f99c8d66c40cc6c0791864589fe7696826430db564a9706e4d68db866d2" => :el_capitan_or_later
+  end
+
+  patch do
+    url "https://github.com/apache/arrow/commit/59c69aa2ec377e03388ff509eb7742e4fd625e01.patch"
+    sha256 "d82a266c0f4f800d30db89c2533413a10a8ee599649ffebdb989f06f0d4e13bb"
   end
 
   depends_on "autoconf" => :build
   depends_on "cmake" => :build
+  depends_on "flatbuffers" => :build
+  depends_on "double-conversion"
   depends_on "boost"
-  depends_on "flatbuffers"
   depends_on "lz4"
-  depends_on "protobuf"
-  depends_on "snappy"
   depends_on "thrift"
-  depends_on "zstd"
 
   def install
     ENV.cxx11
     args = %W[
       -DARROW_PARQUET=ON
       -DARROW_PLASMA=OFF
-      -DARROW_PROTOBUF_USE_SHARED=ON
+      -DARROW_HDFS=OFF
+      -DARROW_BUILD_TESTS=OFF
+      -DARROW_TEST_LINKAGE="static"
+      -DARROW_BUILD_SHARED=OFF
+      -DARROW_JEMALLOC=OFF
+      -DARROW_WITH_BROTLI=OFF
+      -DARROW_USE_GLOG=OFF 
       -DARROW_PYTHON=OFF
+      -DARROW_WITH_ZSTD=OFF
+      -DARROW_WITH_SNAPPY=OFF
       -DFLATBUFFERS_HOME=#{Formula["flatbuffers"].prefix}
       -DLZ4_HOME=#{Formula["lz4"].prefix}
-      -DPROTOBUF_HOME=#{Formula["protobuf"].prefix}
-      -DSNAPPY_HOME=#{Formula["snappy"].prefix}
       -DTHRIFT_HOME=#{Formula["thrift"].prefix}
-      -DZSTD_HOME=#{Formula["zstd"].prefix}
     ]
 
     mkdir "build"
