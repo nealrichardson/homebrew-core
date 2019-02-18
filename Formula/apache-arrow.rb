68dc0c44123814eeb4eb619ca4fa10f213f6eb28
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-18 19:41:57 +0100

    add apache-arrow with parquet

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -1,46 +1,49 @@
 class ApacheArrow < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url "https://www.apache.org/dist/arrow/arrow-0.12.0/apache-arrow-0.12.0.tar.gz"
+  url "https://www.apache.org/dyn/closer.cgi?path=arrow/arrow-0.12.0/apache-arrow-0.12.0.tar.gz"
   sha256 "34dae7e4dde9274e9a52610683e78a80f3ca312258ad9e9f2c0973cf44247a98"
-  head "https://github.com/apache/arrow.git"
 
   bottle do
-    cellar :any_skip_relocation
+    cellar :any
+    rebuild 1
     root_url "https://jeroen.github.io/bottles"
-    sha256 "69599ed990e30bd231af794957d0b6ae6f003cc11b4accea4cf830c2205f988b" => :el_capitan_or_later
+    sha256 "b0bdbe5b924832ba7afbeb5f6173fda4f2756cf7f9df3024dd0388734c1ac844" => :el_capitan
   end
 
+  depends_on "autoconf" => :build
   depends_on "cmake" => :build
   depends_on "boost"
-
-  needs :cxx11
+  depends_on "flatbuffers"
+  depends_on "lz4"
+  depends_on "numpy"
+  depends_on "protobuf"
+  depends_on "python"
+  depends_on "snappy"
+  depends_on "thrift"
+  depends_on "zstd"
 
   def install
     ENV.cxx11
-
-    args = std_cmake_args + %w[
-    -DARROW_BUILD_STATIC=ON
-    -DARROW_BUILD_TESTS=OFF
-    -DARROW_PYTHON=OFF
-    -DARROW_BOOST_USE_SHARED=OFF
-    -DARROW_WITH_SNAPPY=OFF
-    -DARROW_WITH_ZSTD=OFF
-    -DARROW_WITH_LZ4=OFF
-    -DARROW_JEMALLOC=OFF
-    -DARROW_BUILD_SHARED=OFF
-    -DARROW_BOOST_VENDORED=OFF
-    -DARROW_WITH_ZLIB=OFF
-    -DARROW_WITH_BROTLI=OFF
-    -DARROW_USE_GLOG=OFF
-    -DPTHREAD_LIBRARY=OFF
-    -DARROW_BUILD_UTILITIES=ON
-    -DARROW_TEST_LINKAGE="static"
-    -DARROW_HDFS=OFF
+    args = %W[
+      -DARROW_ORC=ON
+      -DARROW_PARQUET=ON
+      -DARROW_PLASMA=ON
+      -DARROW_PROTOBUF_USE_SHARED=ON
+      -DARROW_PYTHON=ON
+      -DFLATBUFFERS_HOME=#{Formula["flatbuffers"].prefix}
+      -DLZ4_HOME=#{Formula["lz4"].prefix}
+      -DPROTOBUF_HOME=#{Formula["protobuf"].prefix}
+      -DPYTHON_EXECUTABLE=#{Formula["python"].bin/"python3"}
+      -DSNAPPY_HOME=#{Formula["snappy"].prefix}
+      -DTHRIFT_HOME=#{Formula["thrift"].prefix}
+      -DZSTD_HOME=#{Formula["zstd"].prefix}
     ]
 
-    cd "cpp" do
-      system "cmake", ".", *args
+    mkdir "build"
+    cd "build" do
+      system "cmake", "../cpp", *std_cmake_args, *args
+      system "make"
       system "make", "install"
     end
   end
@@ -48,8 +51,7 @@ class ApacheArrow < Formula
   test do
     (testpath/"test.cpp").write <<~EOS
       #include "arrow/api.h"
-      int main(void)
-      {
+      int main(void) {
         arrow::int64();
         return 0;
       }
