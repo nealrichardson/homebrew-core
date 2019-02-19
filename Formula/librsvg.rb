2062e3f019ebd94126e05c5e9d4b61a72bf27533
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-19 15:16:30 +0100

    Backport librsvg

--- Formula/librsvg.rb
+++ Formula/librsvg.rb
@@ -1,18 +1,15 @@
 class Librsvg < Formula
   desc "Library to render SVG files using Cairo"
   homepage "https://wiki.gnome.org/Projects/LibRsvg"
-  url "https://download.gnome.org/sources/librsvg/2.42/librsvg-2.42.2.tar.xz"
-  sha256 "0c550a0bffef768a436286116c03d9f6cd3f97f5021c13e7f093b550fac12562"
-  revision 2
+  url "https://download.gnome.org/sources/librsvg/2.44/librsvg-2.44.13.tar.xz"
+  sha256 "d2d660bf0c6441d019ae7a7ba96b789facbfb14dc97818908ee03e15ba6bcb8f"
 
   bottle do
-    sha256 "1a8708ea79f5c93c222fd76fdd9226cb09e84b84299dbd775a3f4931d248c892" => :mojave
-    sha256 "9fbc01305791dd1bcc471457267e3f92a14676c748806afb931077b4a5d7cad0" => :high_sierra
-    sha256 "358ee8bd09767179968af8cf3d2b6fe2f0c881ed1a1ca3f033a7fac59cecd409" => :sierra
-    sha256 "6d7418c2911ce65c4895ce5c39e49ff8cac58e6c424295089533189802c868bc" => :el_capitan
+    cellar :any
+    root_url: "https://jeroen.github.io/bottles"
+    sha256 "f39e8bbb5baac4273f55741b44870ccd65143535aefd908c40e25d8cbcb8c7d7" => :el_capitan_or_later
   end
 
-  depends_on "gobject-introspection" => :build
   depends_on "pkg-config" => :build
   depends_on "rust" => :build
   depends_on "cairo"
@@ -20,7 +17,6 @@ class Librsvg < Formula
   depends_on "glib"
   depends_on "libcroco"
   depends_on "pango"
-  depends_on "gtk+3" => :optional
 
   def install
     args = %W[
@@ -28,8 +24,8 @@ class Librsvg < Formula
       --prefix=#{prefix}
       --disable-Bsymbolic
       --enable-tools=yes
-      --enable-pixbuf-loader=yes
-      --enable-introspection=yes
+      --enable-pixbuf-loader=no
+      --enable-introspection=no
     ]
 
     system "./configure", *args
@@ -45,13 +41,6 @@ class Librsvg < Formula
       "gdk_pixbuf_moduledir=#{lib}/gdk-pixbuf-2.0/2.10.0/loaders"
   end
 
-  def post_install
-    # librsvg is not aware GDK_PIXBUF_MODULEDIR must be set
-    # set GDK_PIXBUF_MODULEDIR and update loader cache
-    ENV["GDK_PIXBUF_MODULEDIR"] = "#{HOMEBREW_PREFIX}/lib/gdk-pixbuf-2.0/2.10.0/loaders"
-    system "#{Formula["gdk-pixbuf"].opt_bin}/gdk-pixbuf-query-loaders", "--update-cache"
-  end
-
   test do
     (testpath/"test.c").write <<~EOS
       #include <librsvg/rsvg.h>
@@ -62,21 +51,16 @@ class Librsvg < Formula
       }
     EOS
     cairo = Formula["cairo"]
-    fontconfig = Formula["fontconfig"]
     freetype = Formula["freetype"]
     gdk_pixbuf = Formula["gdk-pixbuf"]
     gettext = Formula["gettext"]
-    glib = Formula["glib"]
     libpng = Formula["libpng"]
     pixman = Formula["pixman"]
     flags = %W[
       -I#{cairo.opt_include}/cairo
-      -I#{fontconfig.opt_include}
       -I#{freetype.opt_include}/freetype2
       -I#{gdk_pixbuf.opt_include}/gdk-pixbuf-2.0
       -I#{gettext.opt_include}
-      -I#{glib.opt_include}/glib-2.0
-      -I#{glib.opt_lib}/glib-2.0/include
       -I#{include}/librsvg-2.0
       -I#{libpng.opt_include}/libpng16
       -I#{pixman.opt_include}/pixman-1
@@ -84,7 +68,6 @@ class Librsvg < Formula
       -L#{cairo.opt_lib}
       -L#{gdk_pixbuf.opt_lib}
       -L#{gettext.opt_lib}
-      -L#{glib.opt_lib}
       -L#{lib}
       -lcairo
       -lgdk_pixbuf-2.0
