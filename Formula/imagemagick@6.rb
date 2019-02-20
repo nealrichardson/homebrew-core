f1ca9d27a7122fb4e5d9e5f1caee35018121c143
Jeroen Ooms <jeroenooms@gmail.com>
2019-02-20 13:38:10 +0100

    copy imagemagick@6 from homebrew/cran

--- Formula/imagemagick@6.rb
+++ Formula/imagemagick@6.rb
@@ -4,17 +4,15 @@ class ImagemagickAT6 < Formula
   # Please always keep the Homebrew mirror as the primary URL as the
   # ImageMagick site removes tarballs regularly which means we get issues
   # unnecessarily and older versions of the formula are broken.
-  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406--6.9.10-11.tar.xz"
-  mirror "https://www.imagemagick.org/download/ImageMagick-6.9.10-11.tar.xz"
-  sha256 "2d1c61999a9b1f663a085d6657cc4db4b1652af6462256d1aa3c467df3e9e6eb"
-  revision 2
+  url "https://dl.bintray.com/homebrew/mirror/imagemagick%406-6.9.9-39.tar.xz"
+  mirror "https://www.imagemagick.org/download/ImageMagick-6.9.9-39.tar.xz"
+  sha256 "56130c16790d8d38c3a1888cc07074d5b50ffb718243e0c56e4c88839edbf931"
   head "https://github.com/imagemagick/imagemagick6.git"
 
   bottle do
-    sha256 "e37c4f6e5761fccb8decb085b95388005e27f394ef6fe6671d072c7301280383" => :mojave
-    sha256 "7fa7338e26a15333f22302f0d666ef6ee5cdbec01ceedd840e5e3d9fb5560cf5" => :high_sierra
-    sha256 "3c81a4c7d9abf68b4a683014b269bf8715039d6ef8ee316342183d3a9afa21b6" => :sierra
-    sha256 "8c66f808de96341237465735a468533c1a604bb6019ad3b03aad18472ae02337" => :el_capitan
+    cellar :any
+    root_url "https://jeroen.github.io/bottles"
+    sha256 "fdf2bda541cd8d20f06864c859c8cc960938d0f92a14f59d0b0bcfa5c559578d" => :el_capitan_or_later
   end
 
   keg_only :versioned_formula
@@ -25,9 +23,7 @@ class ImagemagickAT6 < Formula
   option "with-openmp", "Compile with OpenMP support"
   option "with-perl", "Compile with PerlMagick"
   option "without-magick-plus-plus", "disable build/install of Magick++"
-  option "without-modules", "Disable support for dynamically loadable modules"
   option "without-threads", "Disable threads support"
-  option "with-zero-configuration", "Disables depending on XML configuration files"
 
   deprecated_option "enable-hdri" => "with-hdri"
   deprecated_option "with-gcc" => "with-openmp"
@@ -37,24 +33,24 @@ class ImagemagickAT6 < Formula
   depends_on "libtool"
   depends_on "xz"
 
-  depends_on "freetype" => :recommended
   depends_on "jpeg" => :recommended
   depends_on "libpng" => :recommended
   depends_on "libtiff" => :recommended
+  depends_on "freetype" => :recommended
+  depends_on "fontconfig" => :recommended
+  depends_on "librsvg" => :recommended
+  depends_on "openjpeg" => :recommended
+  depends_on "pango" => :recommended
+  depends_on "webp" => :recommended
 
-  depends_on "fftw" => :optional
-  depends_on "fontconfig" => :optional
-  depends_on "ghostscript" => :optional
-  depends_on "liblqr" => :optional
-  depends_on "librsvg" => :optional
-  depends_on "libwmf" => :optional
   depends_on "little-cms" => :optional
   depends_on "little-cms2" => :optional
+  depends_on "libwmf" => :optional
+  depends_on "liblqr" => :optional
   depends_on "openexr" => :optional
-  depends_on "openjpeg" => :optional
-  depends_on "pango" => :optional
+  depends_on "ghostscript" => :optional
+  depends_on "fftw" => :optional
   depends_on "perl" => :optional
-  depends_on "webp" => :optional
 
   if build.with? "openmp"
     depends_on "gcc"
@@ -71,13 +67,9 @@ class ImagemagickAT6 < Formula
       --disable-silent-rules
       --enable-shared
       --enable-static
+      --without-modules
+      --enable-zero-configuration
     ]
-
-    if build.without? "modules"
-      args << "--without-modules"
-    else
-      args << "--with-modules"
-    end
 
     if build.with? "opencl"
       args << "--enable-opencl"
