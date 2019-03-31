389e6a10ec7dd124ee5e1da5b14bca8d2841fe0d
builder <builder@builders-Mac.local>
2019-03-31 18:11:49 +0200

    add opencv backport

--- Formula/opencv.rb
+++ Formula/opencv.rb
@@ -1,87 +1,64 @@
 class Opencv < Formula
   desc "Open source computer vision library"
   homepage "https://opencv.org/"
-  url "https://github.com/opencv/opencv/archive/3.4.2.tar.gz"
-  sha256 "81dbd5e7e9f8a4c936b94629bf4765745942a1d634ae38ec08bc57b73b28ffc5"
+  url "https://github.com/opencv/opencv/archive/3.4.5.tar.gz"
+  sha256 "0c57d9dd6d30cbffe68a09b03f4bebe773ee44dc8ff5cd6eaeb7f4d5ef3b428e"
 
   bottle do
-    sha256 "75c1eaf814866f9478c92e44e671046305c356cadc6b80bdd925a023a2d36070" => :mojave
-    sha256 "e8a878f981fbd9ae7daf6f6f73be39dcd20baaf34911b1937ef9b91e0d6d889e" => :high_sierra
-    sha256 "7d42a502f14163663c985247cea157a19f714e1fd2641bd5fc2f653f85592473" => :sierra
-    sha256 "3ca5f6be0e49fe59a3b9274ebda00741de5e8265b6de35813fef43c00086148c" => :el_capitan
+    cellar :any_skip_relocation
+    root_url "https://jeroen.github.io/bottles"
+    sha256 "707ce3e3515a2aee5954fb6481b6ca561e7762630f8c46caafe4ca96a3edccf3" => :el_capitan_or_later
   end
 
   depends_on "cmake" => :build
   depends_on "pkg-config" => :build
   depends_on "eigen"
-  depends_on "ffmpeg"
   depends_on "jpeg"
   depends_on "libpng"
   depends_on "libtiff"
-  depends_on "numpy"
-  depends_on "openexr"
-  depends_on "python"
-  depends_on "python@2"
+  depends_on "webp"
+  depends_on "python" => :build
   depends_on "tbb"
-
-  resource "contrib" do
-    url "https://github.com/opencv/opencv_contrib/archive/3.4.2.tar.gz"
-    sha256 "45a52764ebd2558fa0b7fd8dc47379b37dd0956d912abbf7c786228374fdf60d"
-  end
 
   needs :cxx11
 
   def install
     ENV.cxx11
-    ENV.prepend_path "PATH", Formula["python@2"].opt_libexec/"bin"
-
-    resource("contrib").stage buildpath/"opencv_contrib"
-
-    # Reset PYTHONPATH, workaround for https://github.com/Homebrew/homebrew-science/pull/4885
+    ENV.prepend_path "PATH", Formula["python"].opt_libexec/"bin"
     ENV.delete("PYTHONPATH")
-
-    py2_prefix = `python2-config --prefix`.chomp
-    py2_lib = "#{py2_prefix}/lib"
-
-    py3_config = `python3-config --configdir`.chomp
-    py3_include = `python3 -c "import distutils.sysconfig as s; print(s.get_python_inc())"`.chomp
-    py3_version = Language::Python.major_minor_version "python3"
 
     args = std_cmake_args + %W[
       -DCMAKE_OSX_DEPLOYMENT_TARGET=
+      -DBUILD_EXAMPLES=OFF
+      -DBUILD_opencv_apps=OFF
       -DBUILD_JASPER=OFF
-      -DBUILD_JPEG=ON
+      -DBUILD_ZLIB=OFF
+      -DBUILD_JPEG=OFF
+      -DBUILD_WEBP=OFF
       -DBUILD_OPENEXR=OFF
       -DBUILD_PERF_TESTS=OFF
       -DBUILD_PNG=OFF
       -DBUILD_TESTS=OFF
       -DBUILD_TIFF=OFF
-      -DBUILD_ZLIB=OFF
       -DBUILD_opencv_hdf=OFF
       -DBUILD_opencv_java=OFF
       -DBUILD_opencv_text=OFF
       -DOPENCV_ENABLE_NONFREE=ON
-      -DOPENCV_EXTRA_MODULES_PATH=#{buildpath}/opencv_contrib/modules
       -DWITH_1394=OFF
       -DWITH_CUDA=OFF
       -DWITH_EIGEN=ON
-      -DWITH_FFMPEG=ON
+      -DWITH_FFMPEG=OFF
       -DWITH_GPHOTO2=OFF
       -DWITH_GSTREAMER=OFF
       -DWITH_JASPER=OFF
-      -DWITH_OPENEXR=ON
+      -DWITH_OPENEXR=OFF
       -DWITH_OPENGL=OFF
       -DWITH_QT=OFF
       -DWITH_TBB=ON
       -DWITH_VTK=OFF
-      -DBUILD_opencv_python2=ON
-      -DBUILD_opencv_python3=ON
-      -DPYTHON2_EXECUTABLE=#{which "python"}
-      -DPYTHON2_LIBRARY=#{py2_lib}/libpython2.7.dylib
-      -DPYTHON2_INCLUDE_DIR=#{py2_prefix}/include/python2.7
-      -DPYTHON3_EXECUTABLE=#{which "python3"}
-      -DPYTHON3_LIBRARY=#{py3_config}/libpython#{py3_version}.dylib
-      -DPYTHON3_INCLUDE_DIR=#{py3_include}
+      -DWITH_PROTOBUF=OFF
+      -DBUILD_opencv_python2=OFF
+      -DBUILD_opencv_python3=OFF
     ]
 
     if build.bottle?
@@ -90,14 +67,9 @@ class Opencv < Formula
     end
 
     mkdir "build" do
-      system "cmake", "..", *args
-      system "make"
-      system "make", "install"
-      system "make", "clean"
       system "cmake", "..", "-DBUILD_SHARED_LIBS=OFF", *args
       system "make"
-      lib.install Dir["lib/*.a"]
-      lib.install Dir["3rdparty/**/*.a"]
+      system "make", "install"
     end
   end
 
@@ -112,10 +84,5 @@ class Opencv < Formula
     EOS
     system ENV.cxx, "test.cpp", "-I#{include}", "-L#{lib}", "-o", "test"
     assert_equal `./test`.strip, version.to_s
-
-    ["python2.7", "python3"].each do |python|
-      output = shell_output("#{python} -c 'import cv2; print(cv2.__version__)'")
-      assert_equal version.to_s, output.chomp
-    end
   end
 end
