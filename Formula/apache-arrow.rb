26993025305a6160fca8760e8ae48ee7a5191027
builder <builder@builders-Mac.local>
2019-04-02 14:53:29 +0200

    arrow 0.13.0

--- Formula/apache-arrow.rb
+++ Formula/apache-arrow.rb
@@ -1,22 +1,15 @@
 class ApacheArrow < Formula
   desc "Columnar in-memory analytics layer designed to accelerate big data"
   homepage "https://arrow.apache.org/"
-  url "https://www.apache.org/dyn/closer.cgi?path=arrow/arrow-0.12.0/apache-arrow-0.12.0.tar.gz"
-  sha256 "34dae7e4dde9274e9a52610683e78a80f3ca312258ad9e9f2c0973cf44247a98"
+  url "https://archive.apache.org/dist/arrow/arrow-0.13.0/apache-arrow-0.13.0.tar.gz"
+  sha256 "ac2a77dd9168e9892e432c474611e86ded0be6dfe15f689c948751d37f81391a"
 
   bottle do
-    rebuild 3
     cellar :any_skip_relocation
     root_url "https://jeroen.github.io/bottles"
-    sha256 "c02a0f99c8d66c40cc6c0791864589fe7696826430db564a9706e4d68db866d2" => :el_capitan_or_later
+    sha256 "51189c39448926de0809ed7504ab1be0839de03e4669ba64f1bb98bee2dbc7c7" => :el_capitan_or_later
   end
 
-  patch do
-    url "https://github.com/apache/arrow/commit/59c69aa2ec377e03388ff509eb7742e4fd625e01.patch"
-    sha256 "d82a266c0f4f800d30db89c2533413a10a8ee599649ffebdb989f06f0d4e13bb"
-  end
-
-  depends_on "autoconf" => :build
   depends_on "cmake" => :build
   depends_on "flatbuffers" => :build
   depends_on "double-conversion"
